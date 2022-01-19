7b7a43a14fa9aa09adcf1fd0a976a8f59550ae25
mohammadmahdi <mohammadmahdim76@gmail.com>
2022-01-19 18:33:58 -0500

    3rd day codes added.

---
+++ caesar_cipher.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'caesarCipher' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. STRING s
+ *  2. INTEGER k
+ */
+
+string caesarCipher(string s, int k) {
+    
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] >= 97 && s[i] <= 122) {
+            int temp_val = s[i] - 97;
+            int new_char = (temp_val + k) % 26;
+            s[i] = new_char + 97;
+        } else if (s[i] >= 65 && s[i] <= 90) {
+            int temp_val = s[i] - 65;
+            int new_char = (temp_val + k) % 26;
+            s[i] = new_char + 65;
+        }
+    }
+
+    return s;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string s;
+    getline(cin, s);
+
+    string k_temp;
+    getline(cin, k_temp);
+
+    int k = stoi(ltrim(rtrim(k_temp)));
+
+    string result = caesarCipher(s, k);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
