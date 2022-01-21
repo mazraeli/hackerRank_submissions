d155b9c25b10d841acdafd11644ff742fe1f3c7c
mohammadmahdi <mohammadmahdim76@gmail.com>
2022-01-20 21:56:53 -0500

    4th day codes added.

---
+++ grid_challenge.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'gridChallenge' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING_ARRAY grid as parameter.
+ */
+
+string gridChallenge(vector<string> grid) {
+
+    vector<vector< char > > char_arr;
+    for (auto & element : grid) {
+        vector<char> char_arr_t;
+        for (auto & substr : element) {
+            char_arr_t.push_back(substr);
+        }
+        std::sort(char_arr_t.begin(), char_arr_t.end());
+        char_arr.push_back(char_arr_t);
+    }
+    
+    bool is_in_order = true;
+    for (int i = 0; i < char_arr.size(); i++) {
+        for (int j = 0; j < char_arr.size() - 1; j++) {
+            if (char_arr[j][i] > char_arr[j + 1][i])
+                is_in_order = false;
+        }
+        if (!is_in_order)
+            break;
+    }
+
+    return is_in_order ? "YES" : "NO";
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string t_temp;
+    getline(cin, t_temp);
+
+    int t = stoi(ltrim(rtrim(t_temp)));
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string n_temp;
+        getline(cin, n_temp);
+
+        int n = stoi(ltrim(rtrim(n_temp)));
+
+        vector<string> grid(n);
+
+        for (int i = 0; i < n; i++) {
+            string grid_item;
+            getline(cin, grid_item);
+
+            grid[i] = grid_item;
+        }
+
+        string result = gridChallenge(grid);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
---
+++ recursive_digit_sum.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'superDigit' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. STRING n
+ *  2. INTEGER k
+ */
+
+int superDigit(string n, int k) {
+    
+    string super_string = "";
+    int64_t initial_sum = 0;
+    for (auto & element : n) {
+        initial_sum += int(element) - int('0');
+    }
+    initial_sum *= k;
+    super_string = to_string(initial_sum);
+    
+    while (super_string.length() > 1) {
+        int64_t sum = 0;
+        for (auto it = super_string.begin(); it != super_string.end(); it++) {
+            sum += int64_t(*it) - int64_t('0');
+        }
+        super_string = to_string(sum);
+    }
+
+    return std::stoi(super_string);
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    string n = first_multiple_input[0];
+
+    int k = stoi(first_multiple_input[1]);
+
+    int result = superDigit(n, k);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
